ace9531a39d507ff7fdf3798d13b3a5d0bab3a03
Andrea Sama <asama@ucsd.edu>
2021-10-20 15:30:39 -0700

    class 4 and 6

---
+++ class04 copy/class04.R
@@ -0,0 +1,7 @@
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), type=("l"))
+plot(x, sin(x), type=("l"), col=("pink"))
+plot(x, sin(x), type=("l"), col=("pink"), lwd=(3))
+plot(x, sin(x), type=("l"), col=("pink"), lwd=(3), xlab=("my nice text"))
+                                               
---
+++ class06/class06.R
@@ -0,0 +1,6 @@
+#Creating functions
+#student 1
+c(100, 100, 100, 100, 100, 100, 100, 90)
+#student 2
+c(100, NA, 90, 90, 90, 90, 97, 80)
+#now grade all students in an example
